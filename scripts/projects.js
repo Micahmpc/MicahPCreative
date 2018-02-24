76005e27409bef682056961eddddc1fee1e8072d
Micah <micah.mpc@gmail.com>
2018-02-23 17:06:36 -0800

    added js for inpage nav

---
+++ scripts/projects.js
@@ -0,0 +1,37 @@
+var h = document.getElementById("page-nav-block");
+var readout = document.getElementsByClassName("content");
+var stuck = false;
+var stickPoint = getDistance();
+
+function getDistance() {
+  var topDist = h.offsetTop;
+  return topDist;
+}
+
+window.onscroll = function(e) {
+  var distance = getDistance() - window.pageYOffset;
+  var offset = window.pageTOffset;
+  readout.innerHTML = stickPoint + ' ' + distance + ' ' + offset + ' ' + stuck;
+
+  if((distance <= 90) && !stuck) {
+    h.style.position = "fixed";
+    h.style.top = '90px';
+    stuck = true;
+  }
+  else if (stuck && (offset <= stickPoint)) {
+    h.style.position = 'static';
+    stuck = false;
+  }
+}
+
+
+
+
+function rollover(my_image)
+  {
+    my_image.src = "assets/images/homepage/avatar_secret_2.jpg";
+  }
+function mouseaway(my_image)
+  {
+    my_image.src = 'assets/images/homepage/avatar_2.jpg';
+  }
